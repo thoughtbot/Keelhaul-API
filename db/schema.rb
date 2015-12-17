9a238c929dbb3152f3141ef01aff47348356c0a8
Nick Charlton <nick@nickcharlton.net>
2015-12-17 12:56:46 +0000

    Users should exist before it's modified.

--- db/schema.rb
+++ db/schema.rb
@@ -34,12 +34,12 @@ ActiveRecord::Schema.define(version: 20151210162232) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                          null: false
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.string   "encrypted_password", limit: 128
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128
-    t.string   "token"
+    t.string   "token",                          null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
