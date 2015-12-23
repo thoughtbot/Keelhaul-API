0a0b8444c24e902d2aa64ac27beb1f6192e13a9f
Teo Ljungberg <teo@teoljungberg.com>
2015-12-23 16:06:31 +0100

    Add `null: false` to `User#{created,updated}_at`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151223134225) do
+ActiveRecord::Schema.define(version: 20151223145759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,8 +45,8 @@ ActiveRecord::Schema.define(version: 20151223134225) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                          null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.string   "encrypted_password", limit: 128
     t.string   "confirmation_token", limit: 128
     t.string   "remember_token",     limit: 128
